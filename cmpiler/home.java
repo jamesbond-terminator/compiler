7cddf91a52a793949f8c91a4a62aebd9edc4fbf5
jamesbond-terminator <applicationdevelopment760@gmail.com>
2024-10-19 06:16:56 +0530

    Update home.java

--- cmpiler/home.java
+++ cmpiler/home.java
@@ -1,15 +1,16 @@
-package com.jamesbon.cmpiler;
+package com.jamesbond.terminator;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
@@ -17,71 +18,57 @@ import android.widget.TextView;
 public class home extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the correct layout
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // Initialize views
-        TextView textView = view.findViewById(R.id.textView);
-        SearchView searchView = view.findViewById(R.id.searchView);
+        // Initialize views using the inflated view with the correct types
+        TextView textView = view.findViewById(R.id.textView); // Correct type
+        SearchView searchView = view.findViewById(R.id.searchView); // Correct type
         ImageView notificationIcon = view.findViewById(R.id.notificationIcon);
         FrameLayout bookFrame = view.findViewById(R.id.book);
-        RelativeLayout course = view.findViewById(R.id.course);
-        RelativeLayout paper = view.findViewById(R.id.paper);
-        RelativeLayout result = view.findViewById(R.id.result);
+        FrameLayout course = view.findViewById(R.id.course); // This should remain FrameLayout
+        FrameLayout paper = view.findViewById(R.id.paper); // This should remain FrameLayout
+        FrameLayout result = view.findViewById(R.id.result); // This should remain FrameLayout
+        FrameLayout tt = view.findViewById(R.id.time);
 
-        // book fragment
-
+        // Set up click listeners
         bookFrame.setOnClickListener(v -> {
-            Fragment bookFragment = new test();
+            Fragment bookFragment = new book(); // Make sure this class exists
             ((MainActivity) getActivity()).loadFragment(bookFragment);
         });
 
-        // course
-
         course.setOnClickListener(v -> {
-            Fragment courseFragment = new course();
+            Fragment courseFragment = new course(); // Make sure this class exists
             ((MainActivity) getActivity()).loadFragment(courseFragment);
         });
 
-        // paper
-
         paper.setOnClickListener(v -> {
-            Fragment paperFragment = new paper();
+            Fragment paperFragment = new paper(); // Make sure this class exists
             ((MainActivity) getActivity()).loadFragment(paperFragment);
         });
 
-        // result
-
         result.setOnClickListener(v -> {
             String url = "https://coe1.annauniv.edu/home/";
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse(url));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
             startActivity(intent);
         });
 
-        // Show notification fragment
-
         notificationIcon.setOnClickListener(v -> {
-            notificationIcon notificationFragment = new notidialog();
+            notidialog notificationFragment = new notidialog(); // Make sure this class exists
             notificationFragment.setCancelable(true);
             notificationFragment.show(getChildFragmentManager(), "notificationFragment");
         });
 
+        //tt
 
-
-        // Set up the SearchView functionality
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return true;
-            }
+        tt.setOnClickListener(v -> {
+            Fragment ttfragment = new tt();
+            ((MainActivity) getActivity()).loadFragment(ttfragment);
         });
 
-        return view;
+
+        return view; // Return the inflated view
     }
 }
