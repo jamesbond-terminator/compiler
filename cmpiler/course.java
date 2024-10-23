3acf9e18d958dec15b79fa3a2bc546f0ac0ba1cd
jamesbond-terminator <applicationdevelopment760@gmail.com>
2024-10-23 10:56:45 +0530

    Update course.java

--- cmpiler/course.java
+++ cmpiler/course.java
@@ -1,67 +1,53 @@
 package com.jamesbond.terminator;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.VideoView;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class course extends Fragment {
 
-    private VideoView videoView;
+    private WebView webView1, webView2, webView3, webView4, webView5, webView6;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_course, container, false);
 
-        // Initialize VideoView if you plan to use it
+        // Initialize WebViews
+        webView1 = view.findViewById(R.id.webView1);
+        webView2 = view.findViewById(R.id.webView2);
+        webView3 = view.findViewById(R.id.webView3);
+        webView4 = view.findViewById(R.id.webView4);
+        webView5 = view.findViewById(R.id.webView5);
+        webView6 = view.findViewById(R.id.webView6);
 
-        // Set up click listeners
-        setupClickListener(view, R.id.c1, "https://youtu.be/DuSbZoh_7TI?si=NNcnIMr-zFaoainH");
-        setupClickListener(view, R.id.c2, "https://youtu.be/vg_2FkWgL04?si=wfZvOCYd8ZAJM6Ud");
-        setupClickListener(view, R.id.c3, "https://youtu.be/PKFvMIa37tY?si=OwzHOsfTBPEs6scm");
-        setupClickListener(view, R.id.c4, "https://youtu.be/b2opIFiOpGw?si=jT1NRUYDW9cHdrYJ");
-        setupClickListener(view, R.id.c5, "https://youtu.be/xDCj8RM5OTg?si=f05FaVyAkcXxuvPH");
-        setupClickListener(view, R.id.c6, "https://youtu.be/NslZK-3RJXo?si=euNLksWql8MVvmtD");
-        setupClickListener(view, R.id.c7, "https://youtu.be/98wHiXX_jmk?si=JW8tGQCh0veYQnYy");
-        setupClickListener(view, R.id.c8, "https://youtu.be/PlM82pVD6xE?si=EYjxOunu_fkM9pWN");
-        setupClickListener(view, R.id.c9, "https://youtu.be/XGAQgVKw1GI?si=rE3N1nxlC5EcjQbQ");
-        setupClickListener(view, R.id.c10, "https://youtu.be/6x63PnU4Llg?si=O0jL9NWUN-esro2_");
+        // Enable JavaScript
+        enableWebViewSettings(webView1);
+        enableWebViewSettings(webView2);
+        enableWebViewSettings(webView3);
+        enableWebViewSettings(webView4);
+        enableWebViewSettings(webView5);
+        enableWebViewSettings(webView6);
+
+        // Load YouTube video URLs
+        webView1.loadUrl("https://youtu.be/V2KCAfHjySQ?si=7TjxSIjsjIsKzq_9");
+        webView2.loadUrl("<iframe width=\"100%\" height=\"100%  \" src=\"https://www.youtube.com/embed/V2KCAfHjySQ?si=xVE1ybr8jJplsdd7\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>");
+        webView3.loadUrl("https://www.youtube.com/embed/YOUR_VIDEO_ID_3");
+        webView4.loadUrl("https://www.youtube.com/embed/YOUR_VIDEO_ID_4");
+        webView5.loadUrl("https://www.youtube.com/embed/YOUR_VIDEO_ID_5");
+        webView6.loadUrl("https://www.youtube.com/embed/YOUR_VIDEO_ID_6");
 
         return view;
     }
 
-    private void setupClickListener(View view, int linearLayoutId, String url) {
-        LinearLayout linearLayout = view.findViewById(linearLayoutId);
-        if (linearLayout != null) {
-            linearLayout.setOnClickListener(v -> {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(intent);
-            });
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        if (videoView != null && videoView.isPlaying()) {
-            videoView.pause();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (videoView != null) {
-            videoView.start();
-        }
+    private void enableWebViewSettings(WebView webView) {
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
     }
 }
-
